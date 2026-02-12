624c35c1e4012363094e04240726867f28186dfe
KazutoRiyama <brianbluchet@hotmail.fr>
2026-02-12 22:34:40 +0100

    3 main array completed

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-console.log('Hello!');
+const arrSign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+const arrLuck = ['Very unlucky', 'Unlucky', 'lucky', 'very lucky'];
+const arrAdvice = ['have a beer', 'trust no one', 'take time for yourself', 'take it easy', 'go out of your comfort zone'];
