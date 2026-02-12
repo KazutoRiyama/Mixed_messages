ff9438743b722fd8ba14984989fe6ca3913659bb
KazutoRiyama <brianbluchet@hotmail.fr>
2026-02-12 22:52:23 +0100

    random sign added

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-const arrSign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
-const arrLuck = ['Very unlucky', 'Unlucky', 'lucky', 'very lucky'];
-const arrAdvice = ['have a beer', 'trust no one', 'take time for yourself', 'take it easy', 'go out of your comfort zone'];
+const astrology = {
+    _sign: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'],
+    _luck: ['Very unlucky', 'Unlucky', 'lucky', 'very lucky'],
+    _advice: ['have a beer', 'trust no one', 'take time for yourself', 'take it easy', 'go out of your comfort zone'],
+
+    randomSign () {
+        const index = Math.floor(Math.random() * this._sign.length);
+        return this._sign[index];
+    },
+
+
+}
+
+console.log(astrology.randomSign());
