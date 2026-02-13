b5b314f6c214333184fa4e803724cf95f40cceb3
KazutoRiyama <brianbluchet@hotmail.fr>
2026-02-13 16:28:17 +0100

    finished

--- main.js
+++ main.js
@@ -3,12 +3,42 @@ const astrology = {
     _luck: ['Very unlucky', 'Unlucky', 'lucky', 'very lucky'],
     _advice: ['have a beer', 'trust no one', 'take time for yourself', 'take it easy', 'go out of your comfort zone'],
 
-    randomSign () {
-        const index = Math.floor(Math.random() * this._sign.length);
+   
+     // Returns a random sign different from the excluded one (if provided)
+_getRandomSign(exclude = null) {
+        let index;
+        do {
+            index = Math.floor(Math.random() * this._sign.length);
+        }   while (exclude !== null && this._sign[index] === exclude);
         return this._sign[index];
+    },
+    
+    // Returns one good + one bad sign compatibilty
+    getCompatibility() {
+        const good = this._getRandomSign();          //any sign is fine for 'good'
+        const bad = this._getRandomSign(good);       // exclude the sign in 'good'
+
+        return {
+            goodWith: `You have **good compatibility** with: **${good}**`,
+            badWith: `You have **bad compatibility** with: **${bad}**`
+        };
+    },
+    getCompatibilityMessage() {
+        const { goodWith, badWith } = this.getCompatibility();
+        return `${goodWith}\n${badWith}`;
     },
 
 
-}
+    // daily compatibility, luck, advice
+    getDaily() {
+        const sign = this.getCompatibilityMessage();
+        const luck = this._luck[Math.floor(Math.random() * this._luck.length)];
+        const advice = this._advice[Math.floor(Math.random() * this._advice.length)];
 
-console.log(astrology.randomSign());
+        return `${sign}\nYou are ${luck} today, you should ${advice}`
+    }
+};
+
+
+console.log(astrology.getDaily());
+
